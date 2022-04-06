3a2fd5f32a4baca67afdc4fbf6bc7688d6dbeef7
AdmirHujdur <hujdurado@hotmail.com>
2022-04-06 11:50:36 +0200

    Initial commit

---
+++ src/main/java/com/menu/food/FoodApplication.java
@@ -0,0 +1,13 @@
+package com.menu.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApplication.class, args);
+	}
+
+}
